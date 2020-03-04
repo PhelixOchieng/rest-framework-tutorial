4909218c7213b615dc0ae644b5b6d8ed7a0f9b0d
Phelix <dev.phelixochieng@gmail.com>
2020-03-04 14:32:00 +0300

    Used custom user model

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'posts'
+    'posts',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'users.User'
