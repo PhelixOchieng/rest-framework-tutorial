4bbb5404251d539cbf2fbdb410e208dcded5dbab
Phelix <dev.phelixochieng@gmail.com>
2020-03-04 12:05:35 +0300

    Added picture upload

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user, filename)
+
+
 class Post(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
     is_featured = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=user_directory_path)
 
     def __str__(self):
         return self.title
+
+
